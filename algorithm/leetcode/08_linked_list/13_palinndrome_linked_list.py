61470fea2e051bfaaf69e72358abcaf63ebb0ae5
ddu0422 <ddu0422@naver.com>
2021-01-25 08:45:01 +0900

    feat: 13. 팰린드롬 연결 리스트 #solve 2

--- algorithm/leetcode/08_linked_list/13_palinndrome_linked_list.py
+++ algorithm/leetcode/08_linked_list/13_palinndrome_linked_list.py
@@ -25,3 +25,24 @@ class Solution:
                 return False
 
         return True
+
+    def isPalindrome2(self, head: ListNode) -> bool:
+        # solve 2
+        # slow의 위치부터 거꾸로 이동
+        reverse = None
+        # slow runner, fast runner를 이용
+        slow = fast = head
+
+        # 보통 fast runner는 2칸, slow runner는 1칸 이동
+        while fast and fast.next:
+            fast = fast.next.next
+            reverse, reverse.next, slow = slow, reverse, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while reverse and reverse.val == slow.val:
+            slow, reverse = slow.next, reverse.next
+
+        # reverse가 끝까지 가야(None 상태) 팰린드롬을 만족
+        return not reverse
